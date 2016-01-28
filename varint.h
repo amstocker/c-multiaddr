3f91241c7d7ac50d2cc0bc4ef680b7613e37d72f
Andrew Stocker <amstocker@dons.usfca.edu>
2016-01-27 20:03:08 -0800

    started varint more

--- varint.h
+++ varint.h
@@ -2,12 +2,12 @@
 #include <stdint.h>  /* uint8_t, uint62_t */
 
 
-#define ENCODER(SIZE) \
+#define DEFN_ENCODER(SIZE) \
   size_t \
   uvarint_encode##SIZE (uint##SIZE##_t val, uint8_t buf[], size_t bufsize) \
   { \
     size_t i = 0; \
-    for (; i < 8 && i < bufsize; i++) { \
+    for (; i < (SIZE/8) && i < bufsize; i++) { \
       buf[i] = (uint8_t) (val | 0x80); \
       val >>= 7; \
       if (!val) \
@@ -17,13 +17,13 @@
   }
 
 
-#define DECODER(SIZE) \
+#define DEFN_DECODER(SIZE) \
   size_t \
   uvarint_decode##SIZE (uint8_t buf[], size_t bufsize, uint##SIZE##_t *val) \
   { \
     *val = 0; \
     size_t i = 0; \
-    for (; i < 8 && i < bufsize; i++) { \
+    for (; i < (SIZE/8) && i < bufsize; i++) { \
       *val |= ((buf[i] & 0x7f) << (7 * i)); \
       if (!(buf[i] & 0x80)) \
         return i + 1; \
